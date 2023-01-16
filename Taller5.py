6d7bec31c64ba18a2fbf2db2f10a96e1b89b0c67
Arianna <arigcald@espol.edu.ec>
2023-01-15 22:37:24 -0500

    commit

--- Taller5.py
+++ Taller5.py
@@ -1,3 +1,4 @@
+import numpy as np
 import streamlit as st
 import pandas as pd
 #import plotly_express as px
@@ -5,6 +6,7 @@ from streamlit_option_menu import option_menu
 from PIL import Image
 from collections import namedtuple
 from math import radians, acos, asin, cos, sin, atan, degrees, sqrt
+from utiilities import j,qo_darcy,aof,Qb,qo,qo_vogel,qo_standing,qo_ipr_compuesto,IPR_curve,IPR_curve_methods, IPR_Curve
 
 # Insert an icon
 icon = Image.open("Resources/inflow.png")
@@ -27,14 +29,14 @@ body {background-color: #DCE3D5;
 )
 
 # Title of the app
-st.title(" Reservoir App :link:")
+st.title(" PRODUCTION APP :droplet:")
 
 st.write("---")
 
 st.markdown(
     """ This app is used to visualize the results of reservoir in Engineering of Petroleum.
 
-**Python Libraries:** Streamlit, pandas, plotly, PIL.
+*Python Libraries:* Streamlit, pandas, plotly, PIL.
 """
 )
 
@@ -47,7 +49,7 @@ image = Image.open("Resources/yacimiento.jpg")
 st.image(image, width=100, use_column_width=True)
 
 # Insert video
-st.subheader("**RESERVOIR ENGINEERING**")
+st.subheader("*RESERVOIR ENGINEERING*")
 video = open("Resources/yacimientofrancia.mp4", "rb")
 st.video(video)
 
@@ -56,12 +58,121 @@ Logo = Image.open("Resources/logo.jpg")
 st.sidebar.image(Logo)
 
 # Add title to the sidebar section
-st.sidebar.title(":arrow_down: **Navigation**")
+st.sidebar.title(":arrow_down: *Navigation*")
 # Pages
 with st.sidebar:
     options = option_menu(
         menu_title="Menu",
         options=["Home", "Reservoir Potential", "Nodal Analysis (Single phase flow)"],
-        icons=["house", "fuel-pump", "box"],)
+        icons=["house", "server", "pencil-square"],)
 
+# Call web app sections
+if options == "Reservoir Potential":
+    st.set_option('deprecation.showPyplotGlobalUse', False)
+    if st.checkbox("DARCY"):
+        method="Darcy"
+        st.subheader("*Enter input values*")
+        q_test = st.number_input("Enter q_test value: ")
+        pwf_test = st.number_input("Enter pwf_test value: ")
+        pr = st.number_input("Enter pr value: ")
+        pwf = st.number_input("Enter pwf value")
+        pb = st.number_input("Enter pb value")
+        st.subheader("*Show results*")
+        Q_darcy= qo_darcy(q_test, pwf_test, pr, pwf, pb)
+        st.success(f" Q_Darcy = {Q_darcy:.3f} bpd")
+        IP=j(q_test,pwf_test,pr,pb,ef=1,ef2=None)
+        st.success(f" IP = {IP:.3f} bpd/psia")
+        AOF=aof(q_test,pwf_test,pr,pb,ef=1,ef2=None)
+        st.success(f" AOF = {AOF:.3f} bpd")
+        QB= Qb(q_test,pwf,pr,pb,ef=1,ef2=None)
+        st.success(f" QB = {QB:.3f} bpd")
+        st.subheader("*THE IPR CURVE*")
+        pwf1=st.number_input("Enter the first pwf value: ")
+        pwf2=st.number_input("Enter the second pwf value: ")
+        pwf3=st.number_input("Enter the third pwf value: ")
+        pwf4=st.number_input("Enter the fourth pwf value: ")
+        pwf5=st.number_input("Enter the fifth pwf value: ")
+        list_pwf = np.array([pwf1,pwf2,pwf3,pwf4,pwf5])
+        curva = IPR_curve_methods(q_test, pwf_test, pr, list_pwf, pb, method, ef=1, ef2=None)
+        st.pyplot(curva)
+
+    elif st.checkbox("VOGEL"):
+        method = "Vogel"
+        st.subheader("*Enter input values*")
+        q_test = st.number_input("Enter q_test value: ")
+        pwf_test = st.number_input("Enter pwf_test value: ")
+        pr = st.number_input("Enter pr value: ")
+        pwf = st.number_input("Enter pwf value")
+        pb = st.number_input("Enter pb value")
+        st.subheader("*Show results*")
+        Q_Vogel= qo_vogel(q_test, pwf_test, pr, pwf, pb)
+        st.success(f" Q_VOGEL = {Q_Vogel:.3f} bpd")
+        IP=j(q_test,pwf_test,pr,pb,ef=1,ef2=None)
+        st.success(f" IP = {IP:.3f} bpd/psia")
+        AOF=aof(q_test,pwf_test,pr,pb,ef=1,ef2=None)
+        st.success(f" AOF = {AOF:.3f} bpd")
+        QB= Qb(q_test,pwf,pr,pb,ef=1,ef2=None)
+        st.success(f" QB = {QB:.3f} bpd")
+        st.subheader("*THE IPR CURVE*")
+        pwf1 = st.number_input("Enter the first pwf value: ")
+        pwf2 = st.number_input("Enter the second pwf value: ")
+        pwf3 = st.number_input("Enter the third pwf value: ")
+        pwf4 = st.number_input("Enter the fourth pwf value: ")
+        pwf5 = st.number_input("Enter the fifth pwf value: ")
+        list_pwf = np.array([pwf1, pwf2, pwf3, pwf4, pwf5])
+        curva = IPR_curve_methods(q_test, pwf_test, pr, list_pwf, pb, method, ef=1, ef2=None)
+        st.pyplot(curva)
+
+    elif st.checkbox("STANDING"):
+        method = "Standing"
+        st.subheader("*Enter input values*")
+        q_test = st.number_input("Enter q_test value: ")
+        pwf_test = st.number_input("Enter pwf_test value: ")
+        pr = st.number_input("Enter pr value: ")
+        pwf = st.number_input("Enter pwf value")
+        pb = st.number_input("Enter pb value")
+        st.subheader("*Show results*")
+        Q_Stand=qo_standing(q_test, pwf_test, pr, pwf, pb, ef=1, ef2=None)
+        st.success(f" Q_STANDING = {Q_Stand:.3f} bpd")
+        IP=j(q_test,pwf_test,pr,pb,ef=1,ef2=None)
+        st.success(f" IP = {IP:.3f} bpd/psia")
+        AOF=aof(q_test,pwf_test,pr,pb,ef=1,ef2=None)
+        st.success(f" AOF = {AOF:.3f} bpd")
+        QB= Qb(q_test,pwf,pr,pb,ef=1,ef2=None)
+        st.success(f" Q_B = {QB:.3f} bpd")
+        st.subheader("*THE IPR CURVE*")
+        pwf1 = st.number_input("Enter the first pwf value: ")
+        pwf2 = st.number_input("Enter the second pwf value: ")
+        pwf3 = st.number_input("Enter the third pwf value: ")
+        pwf4 = st.number_input("Enter the fourth pwf value: ")
+        pwf5 = st.number_input("Enter the fifth pwf value: ")
+        list_pwf = np.array([pwf1, pwf2, pwf3, pwf4, pwf5])
+        curva = IPR_curve_methods(q_test, pwf_test, pr, list_pwf, pb, method, ef=1, ef2=None)
+        st.pyplot(curva)
+
+    elif st.checkbox("Qo"):
+        st.subheader("*Enter input values*")
+        q_test = st.number_input("Enter q_test value: ")
+        pwf_test = st.number_input("Enter pwf_test value: ")
+        pr = st.number_input("Enter pr value: ")
+        pwf = st.number_input("Enter pwf value")
+        pb = st.number_input("Enter pb value")
+        st.subheader("*Show results*")
+        Qo=qo(q_test, pwf_test, pr, pwf, pb, ef=1, ef2=None)
+        st.success(f" Qo = {Qo:.3f} bpd")
+        IP=j(q_test,pwf_test,pr,pb,ef=1,ef2=None)
+        st.success(f" IP = {IP:.3f} bpd/psia")
+        AOF=aof(q_test,pwf_test,pr,pb,ef=1,ef2=None)
+        st.success(f" AOF = {AOF:.3f} bpd")
+        QB= Qb(q_test,pwf,pr,pb,ef=1,ef2=None)
+        st.success(f" QB = {QB:.3f} bpd")
+        st.subheader("*THE IPR CURVE*")
+        pwf1 = st.number_input("Enter the first pwf value: ")
+        pwf2 = st.number_input("Enter the second pwf value: ")
+        pwf3 = st.number_input("Enter the third pwf value: ")
+        pwf4 = st.number_input("Enter the fourth pwf value: ")
+        pwf5 = st.number_input("Enter the fifth pwf value: ")
+        list_pwf = np.array([pwf1, pwf2, pwf3, pwf4, pwf5])
+        curva=IPR_Curve(q_test,pwf_test,pr,list_pwf,pb,ef=1,ef2=None,ax=None)
+        st.pyplot(curva)
 
--- utiilities.py
+++ utiilities.py
@@ -240,3 +240,32 @@ def IPR_curve_methods(q_test, pwf_test, pr, pwf:list, pb, method, ef=1, ef2=None
     plt.axvline(x=Qb(q_test, pwf_test, pr, pb), color='r', linestyle='--')
     ax.grid()
     plt.show()
+
+def IPR_Curve(q_test, pwf_test, pr, pwf:list, pb, ef=1, ef2=None, ax=None):
+    # Creating Dataframe
+    df = pd.DataFrame()
+    df['Pwf(psia)'] = pwf
+    df['Qo(bpd)'] = df['Pwf(psia)'].apply(lambda x: qo(q_test, pwf_test, pr, x, pb, ef, ef2))
+    fig, ax = plt.subplots(figsize=(20, 10))
+    x = df['Qo(bpd)']
+    y = df['Pwf(psia)']
+    # The following steps are used to smooth the curve
+    X_Y_Spline = make_interp_spline(x, y)
+    X_ = np.linspace(x.min(), x.max(), 500)
+    Y_ = X_Y_Spline(X_)
+    #Build the curve
+    ax.plot(X_, Y_, c='g')
+    ax.set_xlabel('Qo(bpd)', fontsize=14)
+    ax.set_ylabel('Pwf(psia)', fontsize=14)
+    ax.set_title('IPR', fontsize=18)
+    ax.set(xlim=(0, df['Qo(bpd)'].max() + 10), ylim=(0, df['Pwf(psia)'][0] + 100))
+    # Arrow and Annotations
+    plt.annotate(
+    'Bubble Point', xy=(Qb(q_test, pwf_test, pr, pb), pb),xytext=(Qb(q_test, pwf_test, pr, pb) + 100, pb + 100) ,
+    arrowprops=dict(arrowstyle='->',lw=1)
+    )
+    # Horizontal and Vertical lines at bubble point
+    plt.axhline(y=pb, color='r', linestyle='--')
+    plt.axvline(x=Qb(q_test, pwf_test, pr, pb), color='r', linestyle='--')
+    ax.grid()
+    plt.show()
