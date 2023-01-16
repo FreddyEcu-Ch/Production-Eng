f1bfc96e266a50103611296f3956585def0e9f27
ksls2 <kelosori@espol.edu.ec>
2023-01-15 23:52:38 -0500

    tarea#4

--- Program.py
+++ Program.py
@@ -1,12 +1,14 @@
 # Import Libraries
+import numpy as np
 import streamlit as st
 import pandas as pd
 import plotly_express as px
 from streamlit_option_menu import option_menu
 from PIL import Image
+import matplotlib.pyplot as plt
 from collections import namedtuple
 from math import radians, acos, asin, cos, sin, atan, degrees, sqrt
-
+from utiilities import qo, Qb, qo_vogel, qo_darcy, qo_standing, j, aof, IPR_curve_methods, IPR_Curve, pwf_darcy, f_darcy, sg_oil, sg_avg, gradient_avg
 # Insert an icon
 icon = Image.open("Resources/2.jpg")
 
@@ -34,36 +36,196 @@ st.write("---")
 
 # Add information of the app
 st.markdown(
-    """ This app is used to visualize Reservoir potential of different wells, Nodal Analysis (Single Phase flow).
+    """ This app is used to visualize 3D Trajectories of directional wells, to upload csv files, 
+to call data, and to realize basic calculations.
 
-**Python Libraries:** Streamlit, pandas, plotly, PIL.
+*Python Libraries:* Streamlit, pandas, plotly, PIL.
 """
 )
 
 # Add additional information
 expander = st.expander("About")
-expander.write("This app is very useful for production projects")
+expander.write("This app is very useful for drilling projects")
 
 # Insert image
 image = Image.open("Resources/1.jpeg")
 st.image(image, width=100, use_column_width=True)
 
 # Insert video
-st.subheader("**Drilling Fundamentals**")
+st.subheader("*Production Fundamentals*")
 video = open("Resources/production.mp4", "rb")
 st.video(video)
 
 # Add caption
-st.caption("Video about production fundamentals")
+st.caption("Video about Production fundamentals")
 
 # Sidebar
 Logo = Image.open("Resources/ESPOL.png")
 st.sidebar.image(Logo)
 
+# Add title to the sidebar section
+st.sidebar.title(":arrow_down: *Navigation*")
+
 # Pages
 with st.sidebar:
     options = option_menu(
         menu_title="Menu",
-        options=["Home", "Reservoir Potential", "Nodal Analysis (Single Flow)"],
-        icons=["house", "Droplet half", "Clipboard data"],
+        options=["Home", "Reservoir Potential", "Nodal Analysis"],
+        icons=["house", "droplet half", "clipboard data"],
     )
+
+# Call web app sections
+st.set_option('deprecation.showPyplotGlobalUse', False)
+if options == "Reservoir Potential":
+    if st.checkbox("Darcy"):
+        method = "Darcy"
+        st.subheader("*Enter input values*")
+        q_test = st.number_input("Enter Flow test value: ")
+        pwf_test = st.number_input("Enter Bottom hole flowing pressure test value: ")
+        pr = st.number_input("Enter Reservoir Pressure value: ")
+        pwf = st.number_input("Enter Bottom hole flowing pressure value: ")
+        pb = st.number_input("Enter Bubble point pressure value: ")
+        qb = Qb(q_test, pwf_test, pr, pb, ef=1, ef2=None)
+        IP = j(q_test, pwf_test, pr, pb, ef=1, ef2=None)
+        aof = aof(q_test, pwf_test, pr, pb, ef=1, ef2=None)
+        qo = qo_darcy(q_test, pwf_test, pr, pwf, pb, ef=1, ef2=None)
+        param = ["IP", "Qb", "AOF", "Qo"]
+        st.subheader("*Show results from Darcy Method*")
+        st.success(f"{param[0]} -> {IP:.3f} bbl/d/psi")
+        st.success(f"{param[1]} -> {qb:.3f} BPD")
+        st.success(f"{param[2]} -> {aof:.3f} BPD")
+        st.success(f"{param[3]} -> {qo:.3f} BPD")
+        st.subheader("*Show IPR curve*")
+        pwf1 = st.number_input("Enter pwf1 pressure value: ")
+        pwf2 = st.number_input("Enter pwf2 pressure value: ")
+        pwf3 = st.number_input("Enter pwf3 pressure value: ")
+        pwf4 = st.number_input("Enter pwf4 pressure value: ")
+        pwf5 = st.number_input("Enter pwf5 pressure value: ")
+        pwf_list = np.array([pwf1, pwf2, pwf3, pwf4, pwf5])
+        ipr = IPR_curve_methods(q_test, pwf_test, pr, pwf_list, pb, method, ef = 1, ef2 = None)
+        st.pyplot(ipr)
+
+    elif st.checkbox("Voguel"):
+        st.subheader("*Enter input values*")
+        method = "Voguel"
+        q_test = st.number_input("Enter Flow test value: ")
+        pwf_test = st.number_input("Enter Bottom hole flowing pressure test value: ")
+        pr = st.number_input("Enter Reservoir Pressure test value: ")
+        pwf = st.number_input("Enter Bottom hole flowing pressure value: ")
+        pb = st.number_input("Enter Bubble point pressure value: ")
+        qb = Qb(q_test, pwf_test, pr, pb, ef=1, ef2=None)
+        IP = j(q_test, pwf_test, pr, pb, ef=1, ef2=None)
+        aof = aof(q_test, pwf_test, pr, pb, ef=1, ef2=None)
+        qo = qo_vogel(q_test, pwf_test, pr, pwf, pb, ef=1, ef2=None)
+        param = ["IP", "Qb", "AOF", "Qo"]
+        st.subheader("*Show results from Voguel Method*")
+        st.success(f"{param[0]} -> {IP:.3f} bbl/d/psi")
+        st.success(f"{param[1]} -> {qb:.3f} BPD")
+        st.success(f"{param[2]} -> {aof:.3f} BPD")
+        st.success(f"{param[3]} -> {qo:.3f} BPD")
+        st.subheader("*Show IPR curve*")
+        pwf1 = st.number_input("Enter pwf1 pressure value: ")
+        pwf2 = st.number_input("Enter pwf2 pressure value: ")
+        pwf3 = st.number_input("Enter pwf3 pressure value: ")
+        pwf4 = st.number_input("Enter pwf4 pressure value: ")
+        pwf5 = st.number_input("Enter pwf5 pressure value: ")
+        pwf_list = np.array([pwf1, pwf2, pwf3, pwf4, pwf5])
+        ipr = IPR_curve_methods(q_test, pwf_test, pr, pwf_list, pb, method, ef=1, ef2=None)
+        st.pyplot(ipr)
+
+    elif st.checkbox("Standing"):
+        st.subheader("*Enter input values*")
+        method = "Standing"
+        q_test = st.number_input("Enter Flow test value: ")
+        pwf_test = st.number_input("Enter Bottom hole flowing pressure test value: ")
+        pr = st.number_input("Enter Reservoir Pressure test value: ")
+        pwf = st.number_input("Enter Bottom hole flowing pressure value: ")
+        pb = st.number_input("Enter Bubble point pressure value: ")
+        qb = Qb(q_test, pwf_test, pr, pb, ef=1, ef2=None)
+        IP = j(q_test, pwf_test, pr, pb, ef=1, ef2=None)
+        aof = aof(q_test, pwf_test, pr, pb, ef=1, ef2=None)
+        qo = qo_standing(q_test, pwf_test, pr, pwf, pb, ef=1, ef2=None)
+        param = ["J", "Qb", "AOF", "Qo"]
+        st.subheader("*Show results from Standing Method*")
+        st.success(f"{param[0]} -> {IP:.3f} bbl/d/psi")
+        st.success(f"{param[1]} -> {qb:.3f} BPD")
+        st.success(f"{param[2]} -> {aof:.3f} BPD")
+        st.success(f"{param[3]} -> {qo:.3f} BPD")
+        st.subheader("*Show IPR curve*")
+        pwf1 = st.number_input("Enter pwf1 pressure value: ")
+        pwf2 = st.number_input("Enter pwf2 pressure value: ")
+        pwf3 = st.number_input("Enter pwf3 pressure value: ")
+        pwf4 = st.number_input("Enter pwf4 pressure value: ")
+        pwf5 = st.number_input("Enter pwf5 pressure value: ")
+        pwf_list = np.array([pwf1, pwf2, pwf3, pwf4, pwf5])
+        ipr = IPR_curve_methods(q_test, pwf_test, pr, pwf_list, pb, method, ef=1, ef2=None)
+        st.pyplot(ipr)
+
+    elif st.checkbox("General"):
+        st.subheader("*Enter input values*")
+        q_test = st.number_input("Enter Flow test value: ")
+        pwf_test = st.number_input("Enter Bottom hole flowing pressure test value: ")
+        pr = st.number_input("Enter Reservoir Pressure test value: ")
+        pwf = st.number_input("Enter Bottom hole flowing pressure value: ")
+        pb = st.number_input("Enter Bubble point pressure value: ")
+        qb = Qb(q_test, pwf_test, pr, pb, ef=1, ef2=None)
+        IP = j(q_test, pwf_test, pr, pb, ef=1, ef2=None)
+        aof = aof(q_test, pwf_test, pr, pb, ef=1, ef2=None)
+        qo = qo(q_test, pwf_test, pr, pwf, pb, ef=1, ef2=None)
+        param = ["J", "Qb", "AOF", "Qo"]
+        st.subheader("*Show results from General Method*")
+        st.success(f"{param[0]} -> {IP:.3f} bbl/d/psi")
+        st.success(f"{param[1]} -> {qb:.3f} BPD")
+        st.success(f"{param[2]} -> {aof:.3f} BPD")
+        st.success(f"{param[3]} -> {qo:.3f} BPD")
+        st.subheader("*Show IPR curve*")
+        pwf1 = st.number_input("Enter pwf1 pressure value: ")
+        pwf2 = st.number_input("Enter pwf2 pressure value: ")
+        pwf3 = st.number_input("Enter pwf3 pressure value: ")
+        pwf4 = st.number_input("Enter pwf4 pressure value: ")
+        pwf5 = st.number_input("Enter pwf5 pressure value: ")
+        pwf_list = np.array([pwf1, pwf2, pwf3, pwf4, pwf5])
+        ipr = IPR_Curve(q_test, pwf_test, pr, pwf_list, pb, ef=1, ef2=None, ax=None)
+        st.pyplot(ipr)
+
+elif options == "Nodal Analysis":
+    if st.checkbox("Monophase"):
+        st.subheader("*Enter input values*")
+        pr = st.number_input("Enter Reservoir Pressure test value: ")
+        pb = st.number_input("Enter Bubble point pressure value: ")
+        q_test = st.number_input("Enter Flow test value: ")
+        pwf_test = st.number_input("Enter Bottom hole flowing pressure test value: ")
+        THP = st.number_input("Enter THP value: ")
+        wc = st.number_input("Enter Water concentration value: ")
+        sgh2o = st.number_input("Enter SGH2O concentration value: ")
+        API = st.number_input("Enter Density Oil value: ")
+        ID = st.number_input("Enter ID value: ")
+        TVD = st.number_input("Enter TVD value: ")
+        MD = st.number_input("Enter MD value: ")
+        IP = j(q_test, pwf_test, pr, pb, ef=1, ef2=None)
+        aof = aof(q_test, pwf_test, pr, pb, ef=1, ef2=None)
+        sgoil = sg_oil(API)
+        sgavg = sg_avg(API, wc, sgh2o)
+        GradientAvg= gradient_avg(API, wc, sgh2o)
+        q = [0, 750, 1400, 2250, 3000, 3750, 4500, 5250, 6000, 6750, 7500]
+        df = pd.DataFrame()
+        for i in q:
+            pwf = pwf_darcy(q_test, pwf_test, i, pr, pb)
+            f = f_darcy(i, ID, C=120)
+            F = f*MD
+            P_gravedad = GradientAvg*TVD
+            Pf = F*GradientAvg
+            Po = THP + Pf + P_gravedad
+            Psys = Po -pwf
+            df['Q(bpd)'] = i
+            df['pwf(bpd)'] = pwf
+            df['THP'] = THP
+            df['P_gravity(psi)'] = P_gravedad
+            df["f"] = f
+            df["F(ft)"] = F
+            df["Pf(psi)"] = Pf
+            df["Psys(psi)"] = Psys
+
+
+
+
--- utiilities.py
+++ utiilities.py
@@ -173,11 +173,11 @@ def qo(q_test, pwf_test, pr, pwf, pb, ef=1, ef2=None):
 
 
 # IPR Curve
-def IPR_curve(q_test, pwf_test, pr, pwf:list, pb):
+def IPR_Curve(q_test, pwf_test, pr, pwf:list, pb, ef=1, ef2=None, ax=None):
     # Creating Dataframe
     df = pd.DataFrame()
     df['Pwf(psia)'] = pwf
-    df['Qo(bpd)'] = df['Pwf(psia)'].apply(lambda x: qo_ipr_compuesto(q_test, pwf_test, pr, x, pb))
+    df['Qo(bpd)'] = df['Pwf(psia)'].apply(lambda x: qo(q_test, pwf_test, pr, x, pb, ef, ef2))
     fig, ax = plt.subplots(figsize=(20, 10))
     x = df['Qo(bpd)']
     y = df['Pwf(psia)']
@@ -201,7 +201,6 @@ def IPR_curve(q_test, pwf_test, pr, pwf:list, pb):
     plt.axvline(x=Qb(q_test, pwf_test, pr, pb), color='r', linestyle='--')
     ax.grid()
     plt.show()
-
 
 # IPR Curve
 def IPR_curve_methods(q_test, pwf_test, pr, pwf:list, pb, method, ef=1, ef2=None):
@@ -240,3 +239,34 @@ def IPR_curve_methods(q_test, pwf_test, pr, pwf:list, pb, method, ef=1, ef2=None
     plt.axvline(x=Qb(q_test, pwf_test, pr, pb), color='r', linestyle='--')
     ax.grid()
     plt.show()
+
+#Analysis nodal
+def pwf_darcy(q_test, pwf_test, q, pr, pb):
+    pwf = pr - (q / j(q_test, pwf_test, pr, pb))
+    return pwf
+
+# Pwf when Pr < Pb (Saturated reservoir)
+def pwf_vogel(q_test, pwf_test, q, pr, pb):
+    pwf = 0.125 * pr * (-1 + np.sqrt(81 - 80 * q / aof(q_test, pwf_test, pr, pb)))
+    return pwf
+
+# Friction factor (f) from darcy-weishbach equation
+def f_darcy(Q, ID, C=120):
+    f = (2.083 * (((100 * Q)/(34.3 * C))**1.85 * (1 / ID)**4.8655)) / 1000
+    return f
+
+# SGOil using API
+def sg_oil(API):
+    SG_oil = 141.5 / (131.5 + API)
+    return SG_oil
+
+# SG average of fluids
+def sg_avg(API, wc, sg_h2o):
+    sg_avg = wc * sg_h2o + (1-wc) * sg_oil(API)
+    return sg_avg
+
+# Average Gradient using fresh water gradient (0.433 psi/ft)
+def gradient_avg(API, wc, sg_h2o):
+    g_avg = sg_avg(API, wc, sg_h2o) * 0.433
+    return g_avg
+
