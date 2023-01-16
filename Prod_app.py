fa79179ee3c1828ff79522fb92c5350a0642f77f
brycgonz <brycgonz@espol.edu.ec>
2023-01-15 23:06:58 -0500

    tarea 4

--- Prod_app.py
+++ Prod_app.py
@@ -3,7 +3,8 @@ from streamlit_option_menu import option_menu
 import pandas as pd
 from PIL import Image
 import plotly_express as px
-
+from utiilities import j_darcy,j,aof,Qb,qo_darcy,qo_vogel,qo_ipr_compuesto,qo_standing,qo,IPR_curve_methods, IPR_curve, sg_avg,gradient_avg,inflow,fig
+from math import radians, acos, asin, cos, sin, atan, degrees, sqrt
 #Titulo de la aplicación
 st.title("Production Engineering App")
 
@@ -25,4 +26,159 @@ with st.sidebar:
     options=option_menu(menu_title="Menu",
                         options=["Home","Reservoir Potential","Nodal Analysis (Single phase flow)"],
                         icons=["menu-app","bar-chart-line-fill","clipboard2-data-fill"])
+if options== "Reservoir Potential":
+    if st.checkbox("Darcy"):
+        Seleccion=st.selectbox("Select",("Qo","IPR"))
+        if Seleccion=="Qo":
+            st.subheader("Enter input")
+            q_test=st.number_input("Ingrese caudal de prueba Qtest")
+            pwf_test = st.number_input("Ingrese Pwf de prueba Pwftest")
+            pr= st.number_input("Ingrese Presion de reservorio Pr")
+            pwf=st.number_input("Ingrese pwf")
+            pb= st.number_input("Ingrese Presion de burbuja Pb")
+            qod=qo_darcy(q_test, pwf_test, pr, pwf, pb, ef=1, ef2=None)
+            q="Qo Darcy"
+            st.success(f"{q} -> {qod:.3f} degrees")
+        elif Seleccion=="IPR":
+            st.subheader("Enter input")
+            q_test = st.number_input("Ingrese caudal de prueba Qtest")
+            pwf_test = st.number_input("Ingrese Pwf de prueba Pwftest")
+            pr = st.number_input("Ingrese Presion de reservorio Pr")
+            pb = st.number_input("Ingrese Presion de burbuja Pb")
+            #numero = int(st.number_input("Ingrese cuanto pwf hay:"))
+            pwf = [4000,3500,3000,2500,1000,0]
+            curva = IPR_curve_methods(q_test, pwf_test, pr, pwf, pb, 'Darcy')
+            st.pyplot(curva)
 
+
+
+    elif st.checkbox("Vogel"):
+        Seleccion = st.selectbox("Select", ("Qo", "IPR"))
+        if Seleccion == "Qo":
+            st.subheader("Enter input")
+            q_test = st.number_input("Ingrese caudal de prueba Qtest")
+            pwf_test = st.number_input("Ingrese Pwf de prueba Pwftest")
+            pr = st.number_input("Ingrese Presion de reservorio Pr")
+            pwf = st.number_input("Ingrese pwf")
+            pb = st.number_input("Ingrese Presion de burbuja Pb")
+            qod = qo_vogel(q_test, pwf_test, pr, pwf, pb, ef=1, ef2=None)
+            q = "Qo Vogel"
+            st.success(f"{q} -> {qod:.3f} degrees")
+        elif Seleccion=="IPR":
+            st.subheader("Enter input")
+            q_test = st.number_input("Ingrese caudal de prueba Qtest")
+            pwf_test = st.number_input("Ingrese Pwf de prueba Pwftest")
+            pr = st.number_input("Ingrese Presion de reservorio Pr")
+            pb = st.number_input("Ingrese Presion de burbuja Pb")
+            #numero = int(st.number_input("Ingrese cuanto pwf hay:"))
+            pwf = [4000,3500,3000,2500,1000,0]
+            curva = IPR_curve_methods(q_test, pwf_test, pr, pwf, pb, 'Vogel')
+            st.pyplot(curva)
+
+    elif st.checkbox("Standing"):
+        Seleccion = st.selectbox("Select", ("Qo", "IPR"))
+        if Seleccion == "Qo":
+            st.subheader("Enter input")
+            q_test = st.number_input("Ingrese caudal de prueba Qtest")
+            pwf_test = st.number_input("Ingrese Pwf de prueba Pwftest")
+            pr = st.number_input("Ingrese Presion de reservorio Pr")
+            pwf = st.number_input("Ingrese pwf")
+            pb = st.number_input("Ingrese Presion de burbuja Pb")
+            qod = qo_standing(q_test, pwf_test, pr, pwf, pb, ef=1, ef2=None)
+            q = "Qo Standing"
+            st.success(f"{q} -> {qod:.3f} degrees")
+        elif Seleccion=="IPR":
+            st.subheader("Enter input")
+            q_test = st.number_input("Ingrese caudal de prueba Qtest")
+            pwf_test = st.number_input("Ingrese Pwf de prueba Pwftest")
+            pr = st.number_input("Ingrese Presion de reservorio Pr")
+            pb = st.number_input("Ingrese Presion de burbuja Pb")
+            #numero = int(st.number_input("Ingrese cuanto pwf hay:"))
+            pwf = [4000,3500,3000,2500,1000,0]
+            curva = IPR_curve_methods(q_test, pwf_test, pr, pwf, pb, 'Standing')
+            st.pyplot(curva)
+    elif st.checkbox("IPR Compuesto"):
+        Seleccion = st.selectbox("Select", ("Qo", "IPR"))
+        if Seleccion == "Qo":
+            st.subheader("Enter input")
+            q_test = st.number_input("Ingrese caudal de prueba Qtest")
+            pwf_test = st.number_input("Ingrese Pwf de prueba Pwftest")
+            pr = st.number_input("Ingrese Presion de reservorio Pr")
+            pwf = st.number_input("Ingrese pwf")
+            pb = st.number_input("Ingrese Presion de burbuja Pb")
+            ef = st.number_input("Inrese EF1")
+            ef2 = st.number_input("Inrese EF2")
+            qod = qo_ipr_compuesto(q_test, pwf_test, pr, pwf, pb, ef=1, ef2=None)
+            q = "Qo IPR compuesto"
+            st.success(f"{q} -> {qod:.3f} degrees")
+        elif Seleccion == "IPR":
+            st.subheader("Enter input")
+            q_test = st.number_input("Ingrese caudal de prueba Qtest")
+            pwf_test = st.number_input("Ingrese Pwf de prueba Pwftest")
+            pr = st.number_input("Ingrese Presion de reservorio Pr")
+            pb = st.number_input("Ingrese Presion de burbuja Pb")
+            # numero = int(st.number_input("Ingrese cuanto pwf hay:"))
+            pwf = [4000, 3500, 3000, 2500, 1000, 0]
+            curva = IPR_curve_methods(q_test, pwf_test, pr, pwf, pb, 'IPR_compuesto')
+            st.pyplot(curva)
+
+    elif st.checkbox("Todas las condiciones"):
+        Seleccion = st.selectbox("Select", ("Qo", "IPR"))
+        if Seleccion == "Qo":
+            st.subheader("Enter input")
+            q_test = st.number_input("Ingrese caudal de prueba Qtest")
+            pwf_test = st.number_input("Ingrese Pwf de prueba Pwftest")
+            pr = st.number_input("Ingrese Presion de reservorio Pr")
+            pwf = st.number_input("Ingrese pwf")
+            pb = st.number_input("Ingrese Presion de burbuja Pb")
+            ef=st.number_input("Inrese EF1")
+            ef2 = st.number_input("Inrese EF2")
+            qod = qo(q_test, pwf_test, pr, pwf, pb, ef=1, ef2=None)
+            q = "Qo all conditions"
+            st.success(f"{q} -> {qod:.3f} degrees")
+        elif Seleccion=="IPR":
+            st.subheader("Enter input")
+            q_test = st.number_input("Ingrese caudal de prueba Qtest")
+            pwf_test = st.number_input("Ingrese Pwf de prueba Pwftest")
+            pr = st.number_input("Ingrese Presion de reservorio Pr")
+            pb = st.number_input("Ingrese Presion de burbuja Pb")
+            #numero = int(st.number_input("Ingrese cuanto pwf hay:"))
+            pwf = [4000,3500,3000,2500,1000,0]
+            curva = IPR_curve(q_test, pwf_test, pr, pwf, pb)
+            st.pyplot(curva)
+elif options== "Nodal Analysis (Single phase flow)":
+    if st.checkbox("Monofasico"):
+        opcion=st.selectbox("Select",("GEprom fluid","Gradienteprom fluid","Dataframe Inflow"))
+        if opcion=="GEprom fluid":
+            API=st.number_input("Ingrese API")
+            wcut=st.number_input("Ingrese wc")
+            sg_h2o=st.number_input("Ingrese gravedad especifica h2o")
+            ge=sg_avg(API, wcut, sg_h2o)
+            g = "Gravedad especifica del fluido"
+            st.success(f"{g} -> {ge:.3f} degrees")
+
+        elif opcion=="Gradienteprom fluid":
+            API=st.number_input("Ingrese API")
+            wcut=st.number_input("Ingrese wcut")
+            sg_h2o=st.number_input("Ingrese gravedad especifica h2o")
+            ge=gradient_avg(API, wcut, sg_h2o)
+            g = "Gradiente promedio del fluido"
+            st.success(f"{g} -> {ge:.3f} degrees")
+
+        elif opcion=="Dataframe Inflow":
+            Qt=st.number_input("Ingrese Qt")
+            Pwft=st.number_input("Ingrese Pwft")
+            Pr=st.number_input("Ingrese Pr")
+            Pb = st.number_input("Ingrese Pb")
+            THP = st.number_input("Ingrese THP")
+            API = st.number_input("Ingrese API")
+            wcut = st.number_input("Ingrese wcut")
+            sg_h2o = st.number_input("Ingrese sg_h2o")
+            tvd = st.number_input("Ingrese tvd")
+            ID = st.number_input("Ingrese ID")
+            C = st.number_input("Ingrese C")
+            md = st.number_input("Ingrese md")
+            df=inflow(Qt,Pwft,Pr,Pb,THP,API,wcut,sg_h2o,tvd,ID,C,md)
+            st.write(df)
+            curva=fig(df)
+            st.pyplot(curva)
--- utiilities.py
+++ utiilities.py
@@ -240,3 +240,55 @@ def IPR_curve_methods(q_test, pwf_test, pr, pwf:list, pb, method, ef=1, ef2=None
     plt.axvline(x=Qb(q_test, pwf_test, pr, pb), color='r', linestyle='--')
     ax.grid()
     plt.show()
+
+# funciones
+def pwf_darcy(q_test, pwf_test, q, pr, pb):
+    pwf = pr - (q / j(q_test, pwf_test, pr, pb))
+    return pwf
+# Pwf when Pr < Pb (Saturated reservoir)
+def pwf_vogel(q_test, pwf_test, q, pr, pb):
+    pwf = 0.125 * pr * (-1 + np.sqrt(81 - 80 * q / aof(q_test, pwf_test, pr, pb)))
+    return pwf
+# Friction factor (f) from darcy-weishbach equation
+def f_darcy(Q, ID, C=120):
+    f = (2.083 * (((100 * Q)/(34.3 * C))**1.85 * (1 / ID)**4.8655)) / 1000
+    return f
+# SGOil using API
+def sg_oil(API):
+    SG_oil = 141.5 / (131.5 + API)
+    return SG_oil
+# SG average of fluids
+def sg_avg(API, wc, sg_h2o):
+    sg_avg = wc * sg_h2o + (1-wc) * sg_oil(API)
+    return sg_avg
+# Average Gradient using fresh water gradient (0.433 psi/ft)
+def gradient_avg(API, wc, sg_h2o):
+    g_avg = sg_avg(API, wc, sg_h2o) * 0.433
+    return g_avg
+def inflow(Qt,Pwft,Pr,Pb,THP,API,wc,sg_h2o,tvd,ID,C,md):
+    columns = ['Q(bpd)', 'Pwf(psia)','THP(psia)', 'Pgravity(psia)', 'f', 'F(ft)', 'Pf(psia)', 'Po(psia)', 'Psys(psia)']
+    df = pd.DataFrame(columns=columns)
+
+    df[columns[0]] = np.array([0, 750, 1400, 2250, 3000, 3750, 4500, 5250, 6000, 6750, 7500])
+    df[columns[1]] = df['Q(bpd)'].apply(lambda x: pwf_darcy(Qt, Pwft, x, Pr, Pb))
+    df[columns[2]] = THP
+    df[columns[3]] = gradient_avg(API, wc, sg_h2o) * tvd
+    df[columns[4]] = df['Q(bpd)'].apply(lambda x: f_darcy(x, ID, C))
+    df[columns[5]] = df['f'] * md
+    df[columns[6]] = gradient_avg(API, wc, sg_h2o) * df['F(ft)']
+    df[columns[7]] = df['THP(psia)'] + df['Pgravity(psia)'] + df['Pf(psia)']
+    df[columns[8]] = df['Po(psia)'] - df['Pwf(psia)']
+    return df
+
+def fig(df):
+    fig2, ax = plt.subplots(figsize=(18, 10))
+    ax.plot(df['Q(bpd)'], df['Pwf(psia)'], c='red', label='IPR')
+    ax.plot(df['Q(bpd)'], df['Po(psia)'], c='green', label='VLP')
+    ax.plot(df['Q(bpd)'], df['Psys(psia)'], c='b', label='System Curve')
+    ax.set_xlabel('Q(bpd)')
+    ax.set_ylabel('Pwf(psia)')
+    ax.set_xlim(0, df['Q(bpd)'].max() + 1000)
+    ax.set_title('Nodal Analysis')
+    ax.grid()
+    plt.legend()
+    plt.show()
